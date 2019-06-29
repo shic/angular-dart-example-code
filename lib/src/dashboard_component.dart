8842e641f8a3b9667443a7f9935f15b12e20b744
shi <itshilei@gmail.com>
2019-06-29 11:17:10 +0200

    inject navigation service

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -1,13 +1,25 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_tour_of_heroes/src/services/navigation_service.dart';
 
 @Component(
   selector: 'dashboard',
   template: '''
     <div>
       <h2>dashboard</h2>
+      <button (click)="navigate()">navigate</button>
     </div>
   ''',
   directives: [coreDirectives, formDirectives],
 )
-class DashboardComponent {}
+class DashboardComponent {
+  NavigationService _navigationService;
+
+  DashboardComponent(
+    this._navigationService,
+  );
+
+  void navigate() {
+    _navigationService.navigateTo("heroes");
+  }
+}
