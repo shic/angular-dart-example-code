aeffbb34dd1bb3722ed070b5720d1bdbd7d145b2
shi <itshilei@gmail.com>
2019-07-01 12:39:48 +0200

    move navigation service to service folder

--- lib/src/hero_component_plus.dart
+++ lib/src/hero_component_plus.dart
@@ -1,13 +1,13 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
-import 'package:angular_tour_of_heroes/src/navigation_service.dart';
+import 'package:angular_tour_of_heroes/src/services/navigation_service.dart';
 
 @Component(
   selector: 'my-hero-plus',
   template: '''
     <div>
       <h2>plus</h2>
-      <button (click)="hello()">hello navigate</button>
+      <button (click)="navigateToAnotherPage()">hello navigate</button>
       <button (click)="backNavigate()">back navigate</button>
 
     </div>
@@ -19,7 +19,7 @@ class HeroComponentPlus {
 
   HeroComponentPlus(this._navigationService);
 
-  void hello() {
+  void navigateToAnotherPage() {
     _navigationService.navigateTo();
   }
 
--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,7 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_tour_of_heroes/app_component.template.dart' as ng;
-import 'package:angular_tour_of_heroes/src/navigation_service.dart';
+import 'package:angular_tour_of_heroes/src/services/navigation_service.dart';
 
 import 'main.template.dart' as self;
 
