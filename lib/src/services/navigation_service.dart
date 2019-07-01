7c211f790f1ed7ea2e404645f85ff2ca1673a711
shi <itshilei@gmail.com>
2019-07-01 13:11:08 +0200

    added onRouteActivated

--- lib/src/services/navigation_service.dart
+++ lib/src/services/navigation_service.dart
@@ -7,11 +7,28 @@ class NavigationService {
   NavigationService(
     this._router,
     this._location,
-  );
+  ) {
+    print("before");
+    // overrides every route if the language is not supported
+    _router.onRouteActivated.listen((route) {
+      // enforcing scroll to top at every page route
+      print("mamad");
+      //Element domBodyRef = querySelector("body");
+      //domBodyRef.scrollIntoView(ScrollAlignment.TOP);
+    });
+  }
 
   Future<Object> navigateTo(String destination) {
     print("object hello world");
     return _router.navigate(destination);
+  }
+
+  void popUntil(String destinationURL) {
+    _router.navigate(destinationURL);
+  }
+
+  void popAndPushNamed(String destinationURL, {bool isModal: false}) {
+    _router.navigate(destinationURL);
   }
 
   void pop() {
