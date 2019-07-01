c1ef52e1f454dcac4eb7313d10118373e1bf84d1
shi <itshilei@gmail.com>
2019-07-01 11:58:03 +0200

    Use Router

--- lib/src/hero_component_plus.dart
+++ lib/src/hero_component_plus.dart
@@ -1,13 +1,24 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
 
 @Component(
   selector: 'my-hero-plus',
   template: '''
     <div>
       <h2>plus</h2>
+      <button (click)="navigate()">navigate</button>
+
     </div>
   ''',
   directives: [coreDirectives, formDirectives],
 )
-class HeroComponentPlus {}
+class HeroComponentPlus {
+  Router _router;
+
+  HeroComponentPlus(this._router);
+
+  void navigate() {
+    _router.navigate("dash");
+  }
+}
