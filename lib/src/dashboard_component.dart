f6d107d9ebeb5e9d11ecb155be7973b995e4c295
shi <itshilei@gmail.com>
2019-07-02 15:52:32 +0200

    Try to add material_dropdown_select (not work)

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -1,4 +1,5 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 import 'package:angular_tour_of_heroes/src/services/navigation_service.dart';
 
 @Component(
@@ -7,12 +8,29 @@ import 'package:angular_tour_of_heroes/src/services/navigation_service.dart';
     <div>
       <h2>dashboard</h2>
       <button (click)="navigate()">navigate</button>
+      <material-dropdown-select
+          [options]="myOptions"
+          [(selection)]="mySelection"
+          buttonText="{{mySelection}}"
+          >
+      </material-dropdown-select>
     </div>
   ''',
-  directives: [coreDirectives],
+  directives: [
+    coreDirectives,
+    MaterialDropdownSelectComponent,
+    PopupSourceDirective,
+    materialInputDirectives,
+    MaterialCheckboxComponent,
+    MaterialIconComponent,
+  ],
 )
 class DashboardComponent {
   NavigationService _navigationService;
+
+  //https://dart-lang.github.io/angular_components/#/material_dropdown_select
+  final myOptions = [1, 2, 3];
+  int mySelection = 2;
 
   DashboardComponent(
     this._navigationService,
